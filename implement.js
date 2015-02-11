47cbcbe290c85cff44d7c693814ae19b739e0e5a
Danielv123 <tranen.bananen@gmail.com>
2015-02-11 21:14:45 +0100

    Improved progressbar, optimisations to network use

--- implement.js
+++ implement.js
@@ -26,25 +26,37 @@ function test() {
             var status1 = this.responseText;
             if (str.length > 10) str = str.substring(12, 46);
             document.getElementById("demo").innerHTML = (str);
-			progress(20, $('#progressBar'));
+			progress(15, $('#progressBar'));
             console.log("3");
             var status1Pair = -1
             var status1InvalidA = -1
             var status1Pair = status1.search("Unknown Exchange")
             var status1InvalidA = status1.search("ase enter a valid address")
             var status1InvalidA2 = status1.search("Withdrawal Address Specified")
+            var status1Busy = status1.search("right now, sorry")
 
             if (status1Pair > 1) {
                 document.getElementById("demo").innerHTML = ("Error 100 - Invalid exchange pair");
+				progress(100, $('#progressBar'));
+				break
             }
             if (status1InvalidA > 1) {
                 document.getElementById("demo").innerHTML = ("Error 101 - Invalid address");
-            }
+				progress(100, $('#progressBar'));
+				break
+            } ight now, sorry
             if (status1InvalidA2 > 1) {
                 document.getElementById("demo").innerHTML = ("Error 102 - Withdrawal Address Not Specified");
+				progress(100, $('#progressBar'));
+				break
+            }
+			if (status1Busy > 1) {
+                document.getElementById("demo").innerHTML = ("Error 103 - I am busy right now, sorry");
+				progress(100, $('#progressBar'));
+				break
             }
             console.log("4");
-			progress(35, $('#progressBar'));
+			progress(20, $('#progressBar'));
             var status = this.responseText;
             deposithistory()
 
@@ -87,18 +99,21 @@ function deposithistory() {
 
             if (statusDeposit > 1) {
                 document.getElementById("depositInfo").innerHTML = ("Waiting for deposit");
-				progress(50, $('#progressBar'));
+				progress(30, $('#progressBar'));
             }
             if (statusReceived > 1) {
                 document.getElementById("depositInfo").innerHTML = ("Deposit received");
-				progress(75, $('#progressBar'));
+				progress(67,5, $('#progressBar'));
             }
             if (statusCompleted > 1) {
                 document.getElementById("depositInfo").innerHTML = ("Trade complete");
 				progress(100, $('#progressBar'));
+				break
             }
             if (statusFailed > 1) {
                 document.getElementById("depositInfo").innerHTML = ("Error: 418 - Transaction failed");
+				progress(100, $('#progressBar'));
+				break
             }
             setTimeout("deposithistory()", 5000); /*delay*/
         }
